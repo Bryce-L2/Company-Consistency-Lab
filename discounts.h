459fe7b823302fee81fe3143d39b8b87600ab1a5
Bryce-L2 <bryce.lombardo@mcvts.org>
2025-05-20 17:20:12 -0400

    Update discounts.h

--- discounts.h
+++ discounts.h
@@ -1,29 +1,19 @@
+//include guards stuff
 #ifndef DISCOUNTS_H
 #define DISCOUNTS_H
+
 #include <string>
 
-double student(double price) {
-    return price * 0.70;
-}
+//applies the car coupon that is 30% off 
+double car(double price);
 
-double senior(double price) {
-    return price * 0.90;
-}
+//applies the truck coupon that is 10% off
+double truck(double price);
 
-double veteran(double price) {
-    return price * 0.95;
-}
+//applies motorcycle coupon  that is 5% off
+double motorcycle(double price);
 
-double discount(const std::string& code, double price) {
-    if (code == "Car" || code == "car") {
-        return student(price);
-    } else if (code == "Truck" || code == "truck") {
-        return senior(price);
-    } else if (code == "Van" || code == "van") {
-        return veteran(price);
-    } else {
-    return price;
-    }
-}
+//choces wich coupon to use base on what the user enters
+double discount(std::string& code, double price);  
 
 #endif
